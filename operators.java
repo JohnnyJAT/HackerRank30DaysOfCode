a05c358d991448b3b4dee0153f5214dc7089d404
Prashant Malik <30797635+JohnnyJAT@users.noreply.github.com>
2018-05-20 18:06:12 +0530

    Add files via upload

---
+++ operators.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double meal_cost = in.nextDouble();
+        int tip_percent = in.nextInt();
+        int tax_percent = in.nextInt();
+        in.close();
+        
+        double tip_cost;
+            tip_cost = meal_cost*((double)tip_percent/100);
+        // System.out.println( tip_cost);
+        double  tax_cost;
+            tax_cost = meal_cost*((double)tax_percent/100);
+        // System.out.println( tax_cost);
+        double total_cost;
+            total_cost = meal_cost + tip_cost + tax_cost;
+        
+        System.out.println("The total meal cost is "+Math.round(total_cost)+" dollars.");
+       // System.out.println(total_cost); 
+    }
+}
